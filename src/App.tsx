551e804bcce5c332ba11fed78503785c610dc19a
cuellojoaquinn <joacoide.cuello@gmail.com>
2024-11-17 18:50:43 -0300

    Finalizacion de TP-React

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-//rac
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+import { MiPrimerComponent } from "./components/MiPrimerComponente/MiPrimerComponent"
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect"
+import { FormComponent } from "./components/FormComponent/FormComponent"
+import { AppProduct } from "./components/AddProduct/AppProduct"
 
-import Component1 from "./components/component1"
+export const App = () => {
 
-//Buena practica para enviar al main la applicacion
-const App = () => {
-  return (
-    <>
-    <Component1 text={"Texto desde propiedades"} color="red"></Component1>
-    </>
-  )
+    return (
+        <div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
+            {/* <MiPrimerComponent text={"Texto de prueba"} color={"red"} fontSize={5} />
+            <ComponentCounter />
+            <ComponentUseEffect />
+            <FormComponent /> */}
+            <AppProduct />
+        </div>
+    )
 }
-
-export default App
